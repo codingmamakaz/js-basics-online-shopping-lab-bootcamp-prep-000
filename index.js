83c5418b23c3d6f09d3742838b743bd099a7b95f
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-16 23:54:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function placeOrder(cardNumber){
   var cca = [];
   cca.push(cardNumber);
   if (cca === undefined){
-      cca === 0;
+      cca = 0;
     console.log("Sorry, we don't have a credit card on file for you");
   }else{ 
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
