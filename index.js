c80c7d5cb47ed7ae4187d6512e35bc2246fd433e
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-16 19:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,57 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100);
+  var itemName = {};
+  itemName.item = itemPrice;
+  
+  cart.push({[item] : itemPrice});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
+
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log ("Your shopping cart is empty.");
+  } 
+  else if (cart.length === 1){
+      var oneItem = Object.keys(cart[0]);
+      var onePrice =cart[0][oneItem];
+      console.log(`In your cart, you have ${oneItem} at $${onePrice}.`);
+  }
+  else if (cart.length === 2){
+      var firstItem = Object.keys(cart[0]);
+      var firstPrice =cart[0][firstItem];
+      var secondItem = Object.keys(cart[1]);
+      var secondPrice =cart[1][secondItem];
+      console.log(`In your cart, you have ${firstItem} at $${firstPrice} and ${secondItem} at $${secondPrice}.`);
+  }
+  else {
+    var items = [];
+    for (var i = 0; i < (cart.length-1); i++){
+      var item = Object.keys(cart[i]);
+      var price =cart[i][item];
+        items.push(`${item} at $${price}`);
+      }
+        var last_item = Object.keys(cart[cart.length -1]);
+        items.push(`and ${last_item} at $${cart[cart.length -1][last_item]}`);
+        
+        
+          console.log(`In your cart, you have ${items.join(", ")}.`);
+    }
 }
 
-function total() {
-  // write your code here
-}
+  function total() {
+   var allThePrices=[];
+   for (var i = 0; i < (cart.length); i++){
+     var item = Object.keys(cart[i]);
+     var price =cart[i][item];
+     allThePrices.push(price);
+    }
+    var sum = allThePrices.reduce((a, b) => a+b, 0);
+    return sum;
+  }
 
 function removeFromCart(item) {
   // write your code here
