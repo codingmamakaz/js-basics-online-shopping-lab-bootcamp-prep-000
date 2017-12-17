0750bae0d799d8ea4c9eb474f503d92549fe0447
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-17 03:11:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,12 +77,12 @@ function removeFromCart(item) {
 function placeOrder(cardNumber){
   var cca = [];
   cca.push(cardNumber);
-  if (cca === undefined){
-    console.log("Sorry, we don't have a credit card on file for you");
-  }else{ 
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+  if (cca >= 1){
+     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
       cart.length = 0;
       return cart;
+    
+  }else{ 
+    console.log("Sorry, we don't have a credit card on file for you");
   }
 }
-
